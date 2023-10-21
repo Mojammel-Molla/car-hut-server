13b1223265578b43bcf1f13add1208cfa5a54a98
Mojammel Molla <Mojammelmolla897@gmail.com>
2023-10-21 23:01:00 +0600

    Delete method added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -22,26 +22,94 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const productCollection = client.db('toyotaDB').collection('toyota');
+    // await client.connect();
+    const productCollection = client.db('productDB').collection('product');
 
-    // post data
-    app.post('/toyota', async (req, res) => {
+    const cartCollection = client.db('productDB').collection('cart');
+
+    // post data of product
+    app.post('/products', async (req, res) => {
       const newProduct = req.body;
       console.log(newProduct);
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
 
-    // get data
-    app.get('/toyota', async (req, res) => {
-      const cursor = productCollection.find();
-      const result = await cursor.toArray();
+    // add data to cart
+    app.post('/cart', async (req, res) => {
+      const cartData = req.body;
+      console.log(cartData);
+      const result = await cartCollection.insertOne(cartData);
       res.send(result);
     });
 
+    // app.get('/cart/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const result = await cartCollection.findOne({
+    //     _id: id,
+    //   });
+    //   res.send(result);
+    // });
+    app.get('/cart', async (req, res) => {
+      const result = await cartCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // // Product delete from cart
+    // app.delete('/cart/id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await cartCollection.deleteOne(query);
+    //   res.send(result);
+    // });
+
+    // get single product by id
+    app.get('/product/:id', async (req, res) => {
+      const result = await productCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+    // get data for update product
+    app.get('/update/:id', async (req, res) => {
+      const result = await productCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    // update product data
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const product = {
+        $set: {
+          brand: updatedProduct.name,
+          model: updatedProduct.model,
+          photo: updatedProduct.photo,
+          price: updatedProduct.price,
+          category: updatedProduct.category,
+          rating: updatedProduct.rating,
+          description: updatedProduct.description,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
+    // get data of brands
+    app.get('/products', async (req, res) => {
+      const cursor = await productCollection.find({}).toArray();
+      res.send(cursor);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
